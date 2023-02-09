8d2ccdcc13a5a6f3db6dd6ef98cd8284736dad63
kumaroslash <kumar.shivam@oslash.com>
2023-02-10 01:08:27 +0530

    feat: tested

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define s(a) sort(a.begin(), a.end())
+#define rs(a) sort(a.begin(), a.end(), greater<>())
+#define reverse(a) reverse(a.begin(), a.end())
+#define ll long long
+#define IO ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+void xstatics(){
+  cout << "Testing DOne!!";
+}
+
+int main(){
+  IO ll cases;cin>>cases;
+  while(cases--)xstatics();
+}
